dca5b702da45c96bd0578de1d9f733cdae44db74
Cesar <cesar@Cesars-MacBook-Pro.local>
2024-09-26 12:05:39 -0500

    db connmected with neon

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import FeedPrincipal from "@/components/MainFeed/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl text-black">GREAT PROFIT IDEAS</h1>
+    <main className="flex h-screen flex-col items-center p-24">
+      <h1 className="text-4xl text-black p-8">GREAT PROFIT IDEAS</h1>
       <FeedPrincipal />
     </main>
   );
